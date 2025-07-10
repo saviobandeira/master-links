4516c31cc4e2bf9ba9e66f550cd3ac915cd2a3d0
Sávio Bandeira <savio-bandeira@hotmail.com>
2025-07-10 19:58:39 -0300

    feat: add POST route to update external links

--- src/app/api/external-links/route.ts
+++ src/app/api/external-links/route.ts
@@ -1,6 +1,8 @@
-import { NextResponse } from 'next/server';
-import { existsSync, readFileSync } from 'fs';
+import { NextResponse, NextRequest } from 'next/server';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/src/app/api/auth/[...nextauth]/route';
 
 
 type DataItemsType = {
@@ -25,4 +27,86 @@ function readData() {
 export async function GET() {
     const externalLinks = readData();
     return NextResponse.json(externalLinks);
+}
+
+function writeData({
+    category,
+    items,
+}: DataType) {
+    try {
+        const data = readData();
+        data.push({
+            category,
+            items
+        })
+        writeFileSync(dataPath, JSON.stringify(data, null, 2))
+
+        return { success: true };
+
+    } catch (error) {
+        console.error('Failed to write data:', error);
+        return { success: false };
+    };
+}
+
+function isDataItemType(obj: any) {
+    return (
+        obj &&
+        typeof obj === 'object' &&
+        typeof obj.name === 'string' &&
+        typeof obj.url === 'string'
+    );
+}
+
+function isDataType(obj: any) {
+    if (
+        !obj ||
+        typeof obj !== 'object' ||
+        typeof obj.category !== 'string' ||
+        !Array.isArray(obj.items)
+    ) {
+        return false;
+    };
+
+    return obj.items.every(isDataItemType);
+}
+
+export async function POST(req: NextRequest) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return NextResponse.json(
+            { error: 'Authentication required' },
+            { status: 401 , statusText: 'Unauthenticated' }
+        );
+    };
+
+    let body;
+    try {
+        body = await req.json();
+    } catch (error) {
+        console.error('Failed to get JSON body:', error);
+        return NextResponse.json(
+            { error: 'Invalid or missing JSON body' },
+            { status: 400, statusText: 'Bad Request' }
+        );
+    };
+
+    if (!isDataType(body)) {
+        return NextResponse.json(
+            { error: 'Invalid or missing JSON body' },
+            { status: 400, statusText: 'Bad Request' }
+        );
+    };
+
+    const result = writeData(body);
+    if (!result.success) {
+        return NextResponse.json(
+            { error: 'Failed to write data' },
+            { status: 500, statusText: 'Internal Server Error' }
+        );
+    };
+
+    return NextResponse.json({
+        success: true,
+    });
 }
