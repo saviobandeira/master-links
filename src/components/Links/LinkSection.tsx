3727913686c7057b4989e313011ee4d76f46b969
Sávio Bandeira <savio-bandeira@hotmail.com>
2025-07-17 17:16:24 -0300

    refactor: make LinkSection render titles and buttons dynamically from externalLinks

--- src/components/Links/LinkSection.tsx
+++ src/components/Links/LinkSection.tsx
@@ -1,50 +1,57 @@
 import LinkButton from '@/src/components/Links/LinkButton';
+import { getExternalLinks } from '@/src/utils/services/externalLinksService';
 
 
-export default function LinkSection() {
+export default async function LinkSection() {
+    const externalLinks = getExternalLinks();
+
+    const renderItems = (itemIndex: number) => {
+        return (
+            <div className='py-8 flex flex-col gap-4'>
+                {externalLinks[itemIndex].items.map(item => {
+                    return (
+                        <LinkButton
+                            key={item.id}
+                            name={item.name}
+                            url={item.url}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
+
+    const renderTitle = (title: string) => {
+        return title.toLowerCase() === 'default'
+            ? null
+            : <h2 className='font-bold text-center'>
+                {title}
+            </h2>
+    }
+
+    const renderGroup = () => {
+        return (
+            <>
+                {externalLinks.map((externalLink, index) => {
+                    return (
+                        <div
+                            key={externalLink.id}
+
+                        >
+                            {renderTitle(externalLink.category)}
+                            {renderItems(index)}
+                        </div>
+                    );
+                })}
+            </>
+        );
+    }
+
     return (
         <section className='w-full'>
             <div>
-                <div className='flex flex-col gap-4'>
-                    <LinkButton
-                        name='GitHub'
-                        url='https://github.com/saviobandeira'
-                    />
-                    <LinkButton
-                        name='Linkedin'
-                        url='https://www.linkedin.com/in/savio-bandeira-79760620b/'
-                    />
-                    <LinkButton
-                        name='Instagram'
-                        url='https://www.instagram.com/savio.band/'
-                    />
-                    <LinkButton
-                        name='Portifólio Online'
-                        url='https://www.saviobandeira.com'
-                    />
-                </div>
-
-                <div className='flex flex-col gap-4'>
-                    <h2 className='py-8 font-bold uppercase text-center'>Projetos Online</h2>
-                    <LinkButton
-                        name='Link-in-bio customizado'
-                        url='https://paulotrindade.vercel.app/'
-                    />
-                </div>
-
-                <div className='flex flex-col gap-4'>
-                    <h2 className='py-8 font-bold uppercase text-center'>Código-Fonte</h2>
-                    <LinkButton
-                        name='Sistema de auto atendimento pelo Whatsapp' 
-                        url='https://github.com/saviobandeira/pedeja_wppbot'
-                    />
-                    <LinkButton
-                        name='Sistema Full-Stack para autenticação de usuario com FastAPI/JWT'
-                        url='https://github.com/saviobandeira/user-authentication'
-                    />
-                </div>
+                {renderGroup()}
             </div>
         </section>
-
     );
 }
