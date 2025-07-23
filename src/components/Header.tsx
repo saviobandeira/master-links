1eb95cdd45b44dc0bb2e732082f0a799805bd614
Sávio Bandeira <savio-bandeira@hotmail.com>
2025-07-22 22:54:16 -0300

    refactor: use ProfileSection component in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,12 @@
-import Image from 'next/image';
 import AdminLinkButton from '@/src/components/Button/AdminLinkButton';
-import { getProfile } from '@/src/utils/services/profileService';
+import ProfileSection from '@/src/components/ProfileSection';
 
 
 export default function Header() {
-    const profile = getProfile();
-
     return (
-        <header className='flex flex-col items-center pb-8'>
+        <header className='flex flex-col items-center'>
             <AdminLinkButton />
-            <div>
-                <Image
-                    className=' w-24 h-24 rounded-full mb-4'
-                    src='/images/profile-picture.png'
-                    alt='Foto de perfil'
-                    width={800}
-                    height={800}
-                />
-            </div>
-            <h1 className=' font-bold text-lg mb-8'>{profile.name}</h1>
-            <p>{profile.description}</p>    
+            <ProfileSection descriptionIsVisible={true} />
         </header>
     );
 }
